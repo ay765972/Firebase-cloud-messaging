bc24e2d3e67537138bdcc59477d813d341e801b8
ankit <yadav.ankit@thinksys.com>
2022-06-29 13:19:26 +0530

    add

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,34 +1,34 @@
-importScripts('https://www.gstatic.com/firebasejs/4.12.0/firebase-app.js'); 
-importScripts('https://www.gstatic.com/firebasejs/4.12.0/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/4.12.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/4.12.0/firebase-messaging.js"
+);
 
 var config = {
-    apiKey: "AIzaSyCx9i0pLHI402zYdshJQkeqZuMasdasdcy6m0fL0",
-    authDomain: "fir-react-d8d26.firebaseapp.com",
-    databaseURL: "https://fir-react-asdd8d26.firebaseio.com",
-    projectId: "fir-react-d8d2612213",
-    storageBucket: "fir-react-d8d26.apadasdpspot.com123123",
-    messagingSenderId: "1049769121640",
-    asd:"asdasdasasdasdad",
-    wqewqeasd:"wqasdasdasdasdasd"
-  };
-  firebase.initializeApp(config);
- 
-  const messaging = firebase.messaging();
+  apiKey: "AIzaSyCx9i0pLHI402zYdshJQkeqZuMasdasdcy6m0fL0",
+  authDomain: "fir-react-d8d26.firebaseapp.com",
+  databaseURL: "https://fir-react-asdd8d26.firebaseio.com",
+  projectId: "fir-react-d8d2612213",
+  storageBucket: "fir-react-d8d26.apadasdpspot.com123123",
+  messagingSenderId: "1049769121640",
+  asd: "asdasdasasdasdad",
+  wqewqeasd: "wqasdasdasdasdasd",
+};
+firebase.initializeApp(config);
 
-  messaging.onMessage(function(payload)
-{
-    const title = 'Hello , World';
-    const option = {
-        body:payload.data.statusasdasd
-    };
-    return self.registration.showNotification(title,option);
+const messaging = firebase.messaging();
+
+messaging.onMessage(function (payload) {
+  const title = "Hello , World";
+  const option = {
+    body: payload.data.statusasdasd,
+  };
+  return self.registration.showNotification(title, option);
 });
 
-  messaging.setBackgroundMessageHandler(function(payload)
-{
-    const title = 'Hello , World';
-    const option = {
-        body:payload.data.status
-    };
-    return self.registration.showNotification(title,option);
+messaging.setBackgroundMessageHandler(function (payload) {
+  const title = "Hello , World";
+  const option = {
+    body: payload.data.status,
+  };
+  return self.registration.showNotification(title, option);
 });
